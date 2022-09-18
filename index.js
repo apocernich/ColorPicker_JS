c9af9afc0ffaefd05ae80c8a6a02b58fe7a7b0d0
Alex Pocernich <apocernich1@student.cvtc.edu>
2022-09-18 10:23:11 -0500

    fixed random no call

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const inputToArray = () => {
-  let R = document.getElementById('colorR').value;
-  let G = document.getElementById('colorG').value;
-  let B = document.getElementById('colorB').value;
+function inputToArray() {
+  R = parseFloat(document.getElementById('colorR').value);
+  G = parseFloat(document.getElementById('colorG').value);
+  B = parseFloat(document.getElementById('colorB').value);
+
   colorConverterRGBToPy(R, G, B);
-};
+}
 
 const colorConverterRGBToPy = (R, G, B) => {
-  let startingColor = [parseInt(R), parseInt(G), parseInt(B)];
+  let startingColor = [R, G, B];
   let endingColor = [];
   for (let i = 0; i < startingColor.length; i++) {
-    console.log(startingColor[0]);
-    console.log(startingColor[0] + startingColor[1] + startingColor[2]);
     endingColor[i] =
       (1 / (startingColor[0] + startingColor[1] + startingColor[2])) *
       startingColor[i];
-    console.log(startingColor[i]);
-    console.log(endingColor);
   }
+  endingColor[3] = Math.max(...startingColor) / 255;
+
+  console.log(endingColor);
 };
+
+const colorConverterPyToRGB = () => {};
